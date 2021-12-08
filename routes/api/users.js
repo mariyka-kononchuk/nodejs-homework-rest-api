1d22ae21bc01c4a4dcf4795c3894cab15ec5d2f2
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-12-08 14:37:31 +0200

    logout added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { validation, controllerWrapper } = require('../../middlewares');
+const { auth, validation, controllerWrapper } = require('../../middlewares');
 const { users: controller } = require('../../controllers');
 const { joiSignupSchema, joiLoginSchema} = require('../../models/user');
 
 router.post('/signup', validation(joiSignupSchema), controllerWrapper(controller.signup));
 
 router.post('/login', validation(joiLoginSchema), controllerWrapper(controller.login));
+
+router.post('/logout', auth, controllerWrapper(controller.logout));
 
 module.exports = router;
