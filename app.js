2d48cb0a54887053434c6dcb72b59d3843882fd5
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-12-26 13:17:26 +0200

    postman test with image

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const multer = require('multer');
+const path = require('path');
 
 dotenv.config();
 
 const usersRouter = require('./routes/api/users');
 const currentUsersRouter = require('./routes/api/currentUsers');
 const contactsRouter = require('./routes/api/contacts');
+const req = require('express/lib/request');
 
 const app = express();
 
@@ -16,6 +19,30 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+const tempDir = path.join(__dirname, 'temp');
+console.log(__dirname);
+
+const multerConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+  limits: {
+    fileSize: 2048
+  }
+});
+
+//middleware
+const upload = multer({
+  storage:multerConfig
+})
+
+app.post('/api/contacts', upload.single('image'), async (req, res) => {
+  console.log(req.file)
+})
 
 app.use('/api/users', usersRouter);
 app.use('/api/users', currentUsersRouter);
